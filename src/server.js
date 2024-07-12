9e655d11c1362b6cf6785e619cc75462afb794b8
chien <lisa241231@gmail.com>
2024-07-12 21:12:56 +0800

    testing deploy.yaml: test EC2 deploy

--- src/server.js
+++ src/server.js
@@ -6,7 +6,7 @@ const PORT = process.env.PORT || 3000;
 const DB_PWD = process.env.DB_PWD;
 
 app.get('/', (req, res) => {
-  res.send('Pipeline testing(bash).......');
+  res.send('Pipeline testing(bash).......again');
 });
 
 app.get('/db', (req, res) => {
