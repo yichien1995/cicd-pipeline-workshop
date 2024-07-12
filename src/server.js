757ac075417962f4e8a5723f5888701af1aa85af
chien <lisa241231@gmail.com>
2024-07-12 12:35:19 +0800

    test

--- src/server.js
+++ src/server.js
@@ -6,7 +6,7 @@ const PORT = process.env.PORT || 3000;
 const DB_PWD = process.env.DB_PWD;
 
 app.get('/', (req, res) => {
-  res.send('Hello Pipeline');
+  res.send('Hello Pipeline2');
 });
 
 app.get('/db', (req, res) => {
