c2351dacce2a21ae1eed892ae3203d758ca30791
chien <lisa241231@gmail.com>
2024-07-12 20:55:09 +0800

    testing deploy.yaml: test EC2 deploy pm2 testing(bash)

--- src/server.js
+++ src/server.js
@@ -6,7 +6,7 @@ const PORT = process.env.PORT || 3000;
 const DB_PWD = process.env.DB_PWD;
 
 app.get('/', (req, res) => {
-  res.send('Hello Pipeline pm2 testing.......');
+  res.send('Hello Pipeline pm2 testing(bash).......');
 });
 
 app.get('/db', (req, res) => {
